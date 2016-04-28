9ad0cdd5f98b4882a091674c7c26d14e55fb17f7
Felipe Carmona <felipe.carmona@alumnos.usm.cl>
2016-04-27 23:45:22 -0300

    versión final

--- tarea1 parte 1.py
+++ tarea1 parte 1.py
@@ -1,34 +1,174 @@
+#Importaciones necesarias
 import pandas as pd
-import matplotlib as mpl
-mpl.rcParams['font.family'] = 'Open Sans'
 
-#Parte a
+#Cargar datos de entrada
 pd.options.display.mpl_style = 'default'
-data = pd.read_csv('data/data/titanic-train.csv',sep=';')
+data = pd.read_csv('titanic-train.csv', sep=';')
 
-#Parte b
+#Cantidad de items y variables
+cantidad_items, cantidad_variables = data.shape
 
-data.shape
+#Obtener nombres de variables
+nombres_variables = list(data.columns.values)
+
+#Tipo de cada variable, asi como cantidad de valores no nulos
 data.info()
+
+#Informacion estadistica de cada variable
 data.describe()
 
+#Se muestran los primeros 4 items del dataframe
+primeros = data.head()
+#Se muestran los ultimos 4 items del dataframe
+ultimos = data.tail()
+#Se muestran los ítemes 200 al 210
+itemes_200_210 = data[199:210][:]
+#Se repiten los tres pasos anteriores, pero observando solo las variables Sex y Survived
+primeros_sex_survived = data[['Sex','Survived']].head()
+ultimos_sex_survived = data[['Sex','Survived']].tail()
+itemes_200_210_sex_survived = data[['Sex','Survived']][199:210]
 
-#Parte c
-data.tail()
-data.head()
-data[100:110][:]
-data[['Sex','Survived']].tail()
-data[['Sex','Survived']][100:110]
+#Determinar proporcion de hombres y mujeres que sobrevivieron
+hombres = data[data.Sex == 'male']
+proporcion_hombres_sobrevivientes = float(sum(hombres.Survived))/len(hombres)
+mujeres = data[data.Sex == 'female']
+proporcion_mujeres_sobrevivientes = float(sum(mujeres.Survived))/len(mujeres)
 
-#Parte d
-data['Sex'].value_counts()
-data.groupby('Sex').Survived.count()
-data.groupby('Sex').Survived.mean()
-data.groupby('Survived')['Sex'].value_counts()
-grouped_props = data.groupby('Survived')['Sex'].value_counts()
+#Graficar proporcion de hombres y mujeres que sobrevivieron
+data['Survived'] = data['Survived'].replace(1, 'Did survive')
+data['Survived'] = data['Survived'].replace(0, 'Did not survive')
+proporciones1 = data.groupby('Sex')['Survived'].value_counts() / \
+data.groupby('Sex').size()
+grafico1 = proporciones1.unstack().plot(kind='bar').set_ylabel('Proportions')
+
+#Determinar proporcion de sobrevivientes que son hombres y mujeres
+sobrevivientes = data[data.Survived == 1]
+proporcion_sobrevivientes_hombres = float(sum(sobrevivientes.Sex == 'male'))/len(sobrevivientes)
+proporcion_sobrevivientes_mujeres = float(sum(sobrevivientes.Sex == 'female'))/len(sobrevivientes)
+
+#Graficar proporcion de sobrevivientes que son hombres y mujeres
+proporciones2 = data.groupby('Survived')['Sex'].value_counts() / \
 data.groupby('Survived').size()
-grouped_props.unstack().plot(kind='bar')
+grafico2 = proporciones2.unstack().plot(kind='bar')
 
-#Parte e
-data.groupby('Survived')['Age'].mean()
-data.boxplot(column='Age',by='Survived')
+#Edad media de pasajeros que sobreviveron
+edad_media_sobrevivientes = data.groupby('Survived')['Age'].mean()
+#Obtener boxplot de la categoria anterior
+boxplot1 = data.boxplot(column='Age', by='Survived')
+#Obtener histograma de la categoria anterior
+hist1 = data.hist(column='Age', by='Survived')
+
+#Obtener datos del pasajero que posee la edad maxima
+datos_edad_maxima = data[data['Age'] == data['Age'].max()]
+
+#Imputar edades faltantes, usando promedio de edades de cada sexo
+promedio_hombres = data[(data['Age'].notnull()) & (data['Sex'] == 'male')]['Age'].mean()
+promedio_mujeres = data[(data['Age'].notnull()) & (data['Sex'] == 'female')]['Age'].mean()
+data.loc[(data['Age'].isnull()) & (data['Sex'] == 'male'), 'Age'] = promedio_hombres
+data.loc[(data['Age'].isnull()) & (data['Sex'] == 'female'), 'Age'] = promedio_mujeres
+
+#Determinar cantidad de clases diferentes de pasajeros
+cantidad_clases = data['Pclass'].unique()
+
+#Proporcion de sobrevivientes por cada clase
+clase1 = data[data.Pclass == 1]
+proporcion_clase1_sobrevivientes = float(sum(clase1.Survived))/len(clase1)
+clase2 = data[data.Pclass == 2]
+proporcion_clase2_sobrevivientes = float(sum(clase2.Survived))/len(clase2)
+clase3 = data[data.Pclass == 3]
+proporcion_clase3_sobrevivientes = float(sum(clase3.Survived))/len(clase3)
+#Construir grafico para mostrar la informacion anterior
+proporciones3 = data.groupby('Pclass')['Survived'].value_counts() / \
+data.groupby('Pclass').size()
+grafico3 = proporciones3.unstack().plot(kind='bar').set_ylabel('Proportions')
+
+#Proporcion de hombres sobrevivientes por cada clase
+hombres_sobrevivientes_xclase = data[data.Sex == 'male'].groupby(['Survived', 'Pclass']).size()/ \
+data[data.Sex == 'male'].groupby(['Survived']).size()
+hombres_sobrevivientes_xclase.unstack().plot(kind='bar', title='Males').set_ylabel('Proportions')
+
+#Proporcion de mujeres sobrevivientes por cada clase
+mujeres_sobrevivientes_xclase = data[data.Sex == 'female'].groupby(['Survived', 'Pclass']).size()/ \
+data[data.Sex == 'female'].groupby(['Survived']).size()
+mujeres_sobrevivientes_xclase.unstack().plot(kind='bar', title='Females').set_ylabel('Proportions')
+
+#Construccion de regla de prediccion de sobrevivencia en base a sexo y clase de pasajero
+#Se construye nueva columna
+data['prediction'] = 0
+#Se predicen los sobrevivientes
+data.loc[(data.Sex == 'female') & (data.Pclass == 1), 'prediction'] = 1
+nuevo_orden = [11] + range(11)
+data.reindex(columns=data.columns[nuevo_orden])
+#Se calcula precision para sobrevivientes
+precision1 = data[data.prediction == 1][data.Survived == 'Did survive'].size / float(data[data.prediction == 1].size)
+#Se calcula precision para no sobrevivientes
+precision2 = data[data.prediction == 0][data.Survived == 'Did not survive'].size / float(data[data.prediction == 0].size)
+#Se calcula recall para sobrevivientes
+recall1 = data[data.prediction == 1][data.Survived == 'Did survive'].size / float(data[data.Survived == 'Did survive'].size)
+#Se calcula recall para no sobrevivientes
+recall2 = data[data.prediction == 0][data.Survived == 'Did not survive'].size / float(data[data.Survived == 'Did not survive'].size)
+#Guardar archivo con nueva columna
+data.to_csv('predicciones-titanic.csv', index=False)
+
+#Boxplot para precio del boleto (variable Fare)
+data.boxplot(column='Fare', by='Survived')
+#Filtrar outliers
+data_filt = data[data.Fare < 52]
+#Histograma de data_filt
+hist2 = data_filt.hist(column='Fare', by='Survived')
+
+#Creacion de variable rango de precio
+data['rangoPrecio'] = ''
+#Se asigna un rango para la variable creada en cada ítem
+data.loc[(data.Fare >= 0) & (data.Fare <= 9), 'rangoPrecio'] = '[0,9]'
+data.loc[(data.Fare >= 10) & (data.Fare <= 19), 'rangoPrecio'] = '[10,19]'
+data.loc[(data.Fare >= 20) & (data.Fare <= 29), 'rangoPrecio'] = '[20,29]'
+data.loc[(data.Fare >= 29) & (data.Fare <= 39), 'rangoPrecio'] = '[29,39]'
+data.loc[data.Fare >= 40, 'rangoPrecio'] = '[40+]'
+nuevo_orden = [12] + range(12)
+data.reindex(columns=data.columns[nuevo_orden])
+
+#Se abre conjunto de datos de entrenamiento
+datatest = pd.read_csv('/home/Seba/AID/Tarea1/titanic-test.csv', sep=',')
+d = pd.read_csv('/home/Seba/AID/Tarea1/gendermodel.csv', sep=',')
+columna_survived = d['Survived']
+datatest['Survived'] = 0
+datatest['Survived'] = columna_survived
+#Se agrega columna predictiva
+datatest['prediction'] = 0
+#Se predicen los sobrevivientes
+datatest.loc[(datatest.Sex == 'female') & (datatest.Pclass == 1), 'prediction'] = 1
+
+nuevo_orden = [11] + range(11)
+datatest.reindex(columns=data.columns[nuevo_orden])
+
+#Se calcula precision y recall sobre conjunto de datos de entrenamiento
+#Se calcula precision para sobrevivientes
+precision3 = datatest[datatest.prediction == 1][datatest.Survived == 1].size / float(datatest[datatest.prediction == 1].size)
+#Se calcula precision para no sobrevivientes
+precision4 = datatest[datatest.prediction == 0][datatest.Survived == 0].size / float(datatest[datatest.prediction == 0].size)
+#Se calcula recall para sobrevivientes
+recall3 = datatest[datatest.prediction == 1][datatest.Survived == 1].size / float(datatest[datatest.Survived == 1].size)
+#Se calcula recall para no sobrevivientes
+recall4 = datatest[datatest.prediction == 0][datatest.Survived == 0].size / float(datatest[datatest.Survived == 0].size)
+#Guardar archivo con nueva columna
+data.to_csv('predicciones-test-titanic.csv', index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tarea1 parte 2.py
+++ tarea1 parte 2.py
@@ -3,7 +3,9 @@ import numpy as np
 import matplotlib.pyplot as  plt
 from sklearn.preprocessing import StandardScaler
 import math
-"""
+
+
+
 #Parte  (a)
 #Se limpia el dataframe, se eliminan los anos 1979 al 1989
 #Se eliminan los valores nulos del  ano 2010
@@ -19,11 +21,13 @@ data.columns.names = ['year']
 #Parte (b)
 #Grafico que muestra evolucion del VIH en distintos paises
 fig, ax = plt.subplots(figsize=(8, 4))
-data.loc[['Chile','Argentina','Italy','Cameroon', 'Congo, Rep,'],'1990':].T.plot(ax=ax)
+data.loc[['Chile','Canada','Italy','Portugal','Japan','South Korea','Angola','Egypt','Australia','New Zealand'],'1990':].T.plot(ax=ax)
 ax.legend(loc='upper left', bbox_to_anchor=(0.1, 1.1),prop={'size':'x-small'},ncol=6)
 plt.tight_layout(pad=1.5)
 #plt.show()
 
+
+"""
 #Parte (c)
 X = data.ix[:,'1990':'2011'].values
 X_std = StandardScaler().fit_transform(X)
@@ -41,6 +45,7 @@ valores, vectores = np.linalg.eig(cov_mat)
 #Realizado con SVD
 U,s,V =np.linalg.svd(cov_mat)
 eig_pairs = [(np.abs(s[i]), U[:,i]) for i in range(len(valores))]
+print eig_pairs
 eig_pairs.sort()
 eig_pairs.reverse()
 
@@ -51,11 +56,11 @@ cum_var_exp = np.cumsum(var_exp)
 plt.figure(figsize=(6, 4))
 var_exp=var_exp[:4]
 cum_var_exp=cum_var_exp[:4]
-plt.bar(range(4), var_exp, alpha=0.5, align='center',label='individual explained variance')
+plt.bar(range(4), var_exp, alpha=0.5, align='center',label='Varianza individual',color='Red')
 plt.step(range(4), cum_var_exp, where='mid',
-         label='cumulative explained variance')
-plt.ylabel('Explained variance ratio')
-plt.xlabel('Principal components')
+         label='Varianza acumulada')
+plt.ylabel('Varianza')
+plt.xlabel('Componentes principales')
 plt.legend(loc='best')
 plt.tight_layout()
 
@@ -68,6 +73,7 @@ matrix_w = np.hstack((eig_pairs[0][1].reshape(22,1),
 Y = X_std.dot(matrix_w)
 
 
+
 data_2d = pd.DataFrame(Y)
 data_2d.index = data.index
 data_2d.columns = ['PC1','PC2']
@@ -75,25 +81,32 @@ data_2d.columns = ['PC1','PC2']
 #Parte (e)
 #Plot Scatter por promedio
 row_means = tuple(data.mean(axis=1))
-data_2d.plot(kind='scatter', x='PC2', y='PC1', figsize=(16,8), c=row_means,cmap='Reds')
+row_means_n=data.mean(axis=1)
+#data_2d.plot(kind='scatter', x='PC2', y='PC1', figsize=(16,8), c=row_means,cmap='Reds')
 
 
-#Plot Scatter por diferencia entre los anos
-row_trends=tuple(data.diff(1).mean(axis=1))
-data_2d.plot(kind='scatter', x='PC2', y='PC1', figsize=(16,8), c=row_trends,cmap='RdBu')
+#Plot Scatter por diferencia entre los anios
+#print data
+row_trends=tuple(np.diff(data).mean(axis=1))
+row_trends_n =np.diff(data).mean(axis=1)
+#data_2d.plot(kind='scatter', x='PC2', y='PC1', figsize=(16,8), c=row_trends,cmap='RdBu')
 
 #Parte (f)
 #Scatters Modificado
-#data_2d.plot(kind='scatter', x='PC2', y='PC1', figsize=(16,8), s=10000*row_means,c=row_means)
-#data_2d.plot(kind='scatter', x='PC2', y='PC1', figsize=(16,8), s=10000*row_trends,c=row_trends)
+data_2d.plot(kind='scatter', x='PC2', y='PC1', figsize=(16,8), s=row_means_n*1000,c=row_means,cmap='Reds')
+data_2d.plot(kind='scatter', x='PC2', y='PC1', figsize=(16,8), s=10000*row_trends_n,c=row_trends,cmap='RdBu')
+
+
 
 
 #Parte(g)
+
 fig, ax = plt.subplots(figsize=(16,8))
 row_means = tuple(data.mean(axis=1))
-row_trends = tuple(data.diff(1).mean(axis=1))
-print row_means
-data_2d.plot(kind='scatter', x='PC2', y='PC1', ax=ax, s=10*row_means, c=row_means, cmap='RdBu')
+row_trends=tuple(np.diff(data).mean(axis=1))
+
+#Grafico 1
+data_2d.plot(kind='scatter', x='PC2', y='PC1', ax=ax, s=1000*row_means_n, c=row_means, cmap='Reds')
 Q3_HIV_world = data.mean(axis=1).quantile(q=0.85)
 HIV_country = data.mean(axis=1)
 names = data.index
@@ -103,9 +116,32 @@ for i, txt in enumerate(names):
 
 plt.xlabel('Principal Component 1')
 plt.ylabel('Principal Component 2')
+
+
+
+fig, ax = plt.subplots(figsize=(16,8))
+row_means = tuple(data.mean(axis=1))
+row_trends=tuple(np.diff(data).mean(axis=1))
+
+#Grafico 2
+data_2d.plot(kind='scatter', x='PC2', y='PC1', ax=ax, s=10000*row_trends_n, c=row_trends, cmap='RdBu')
+Q3_HIV_world = pd.DataFrame(np.diff(data)).mean(axis=1).quantile(q=0.85)
+HIV_country = pd.DataFrame(np.diff( data)).mean(axis=1)
+names = data.index
+for i, txt in enumerate(names):
+    if(HIV_country[i]>Q3_HIV_world):
+        ax.annotate(txt, (data_2d.iloc[i].PC2+0.2,data_2d.iloc[i].PC1))
+
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+
+
+
+
+
+
 plt.show()
 
-"""
 
 
 #######################################
@@ -122,8 +158,8 @@ cov_mat = np.transpose(X_std).dot(X_std) / (X_std.shape[0]-1)
 valores, vectores = np.linalg.eig(cov_mat)
 
 eig_pairs = [(np.abs(valores[i]), vectores[:,i]) for i in range(len(valores))]
-eig_pairs.sort()
 eig_pairs.reverse()
+eig_pairs.sort()
 
 #Grafico  varianzas
 tot = sum(valores)
@@ -132,14 +168,14 @@ cum_var_exp = np.cumsum(var_exp)
 plt.figure(figsize=(6, 4))
 var_exp=var_exp[:4]
 cum_var_exp=cum_var_exp[:4]
-plt.bar(range(4), var_exp, alpha=0.5, align='center',label='individual explained variance')
+plt.bar(range(4), var_exp, alpha=0.5, align='center',label='Varianza individual')
 plt.step(range(4), cum_var_exp, where='mid',
-         label='cumulative explained variance')
-plt.ylabel('Explained variance ratio')
-plt.xlabel('Principal components')
+         label='Varianza acumulada',color = 'Red')
+plt.ylabel('Varianza')
+plt.xlabel('Componentes principales')
 plt.legend(loc='best')
 plt.tight_layout()
-plt.show()
+
 
 #Proyeccion  matriz
 matrix_w = np.hstack((eig_pairs[0][1].reshape(18,1),
@@ -153,24 +189,35 @@ data_2d.columns = ['PC1','PC2']
 #Parte (e)
 #Plot Scatter por promedio
 row_means = tuple(data.mean(axis=1))
+row_means_n = data.mean(axis=1)
 data_2d.plot(kind='scatter', x='PC2', y='PC1', figsize=(16,8), c=row_means,cmap='Reds')
 
 
 #Plot Scatter por diferencia entre los anos
-row_trends=tuple(data.diff(1).mean(axis=1))
+row_trends=tuple(np.diff(data).mean(axis=1))
+row_trends_n =np.diff(data).mean(axis=1)
 data_2d.plot(kind='scatter', x='PC2', y='PC1', figsize=(16,8), c=row_trends,cmap='RdBu')
+
 
 #Parte (f)
 #Scatters Modificado
-#data_2d.plot(kind='scatter', x='PC2', y='PC1', figsize=(16,8), s=10000*row_means,c=row_means)
-#data_2d.plot(kind='scatter', x='PC2', y='PC1', figsize=(16,8), s=10000*row_trends,c=row_trends)
+data_2d.plot(kind='scatter', x='PC2', y='PC1', figsize=(16,8), s=10*row_means_n,c=row_means, cmap='Reds')
+data_2d.plot(kind='scatter', x='PC2', y='PC1', figsize=(16,8), s=50*row_trends_n,c=row_trends,cmap='RdBu')
+
+
 
 
 #Parte(g)
+#Grafico 1
+
 fig, ax = plt.subplots(figsize=(16,8))
 row_means = tuple(data.mean(axis=1))
-row_trends = tuple(data.diff(1).mean(axis=1))
-data_2d.plot(kind='scatter', x='PC2', y='PC1', ax=ax, s=10*row_means, c=row_means, cmap='RdBu')
+row_means_n = data.mean(axis=1)
+
+row_trends=tuple(pd.DataFrame(np.diff(data)).mean(axis=1))
+row_trends_n=pd.DataFrame(np.diff(data)).mean(axis=1)
+
+data_2d.plot(kind='scatter', x='PC2', y='PC1', ax=ax, s=10*row_means_n, c=row_means, cmap='Reds')
 Q3_HIV_world = data.mean(axis=1).quantile(q=0.85)
 HIV_country = data.mean(axis=1)
 names = data.index
@@ -180,4 +227,19 @@ for i, txt in enumerate(names):
 
 plt.xlabel('Principal Component 1')
 plt.ylabel('Principal Component 2')
+
+#Grafico 2
+fig, ax = plt.subplots(figsize=(16,8))
+data_2d.plot(kind='scatter', x='PC2', y='PC1', ax=ax, s=50*row_trends_n, c=row_trends, cmap='RdBu')
+Q3_HIV_world = pd.DataFrame(np.diff(data)).mean(axis=1).quantile(q=0.85)
+HIV_country = pd.DataFrame(np.diff(data)).mean(axis=1)
+names = data.index
+for i, txt in enumerate(names):
+    if(HIV_country[i]>Q3_HIV_world):
+        ax.annotate(txt, (data_2d.iloc[i].PC2+0.2,data_2d.iloc[i].PC1))
+
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+
+"""
 plt.show()
